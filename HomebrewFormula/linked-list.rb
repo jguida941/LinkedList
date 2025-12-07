b568b3cb10e7b83a6c8e98b175571f9a32199cac
Justin <justin.guida@snhu.edu>
2025-12-07 10:16:30 -0500

    Update Homebrew formula for v1.1.0

--- HomebrewFormula/linked-list.rb
+++ HomebrewFormula/linked-list.rb
@@ -1,15 +1,14 @@
 class LinkedList < Formula
   desc "C++ linked list demo with CSV loader - supports dark/light terminal themes"
   homepage "https://github.com/jguida941/LinkedList"
-  url "https://github.com/jguida941/LinkedList/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "c73a2851467e391b8fcc81548fc3ec72a2ec1b2741572892659a969e9fd2cd58"
-  version "1.0.0"
+  url "https://github.com/jguida941/LinkedList/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "4add7c060c32a16a9751c2ce574fec5bbd804885b83ba6da35a722323e355639"
+  version "1.1.0"
   head "https://github.com/jguida941/LinkedList.git", branch: "main"
 
   depends_on "cmake" => :build
 
   def install
-    # Source files are now in src/ directory
     system "cmake", "-S", ".", "-B", "build", "-DCMAKE_BUILD_TYPE=Release"
     system "cmake", "--build", "build", "--config", "Release"
     bin.install "build/Linked_List" => "linked-list"
@@ -27,7 +26,6 @@ class LinkedList < Formula
   end
 
   test do
-    # Run interactively with a single input to exit (menu option 9)
-    pipe_output("#{bin}/linked-list #{pkgshare}/eBid_Monthly_Sales.csv 98109", "9\n", 0)
+    pipe_output("#{bin}/linked-list #{pkgshare}/eBid_Monthly_Sales.csv", "9\n", 0)
   end
 end
